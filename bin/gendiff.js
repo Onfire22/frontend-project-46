822cc8bc1ad3ecdaf5628e838c9b0ec39a98aadb
Максим Некрасов <maddmax22@yandex.ru>
2024-03-21 12:29:23 +0300

    second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,4 +8,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .parse(process.argv);
