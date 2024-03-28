e31ad797f4205ec83e5b32d31cfeea4ac80650bf
Максим Некрасов <maddmax22@yandex.ru>
2024-03-28 15:28:46 +0300

    build tree

--- src/flatDiff.js
+++ src/flatDiff.js
@@ -12,11 +12,11 @@ const getUniqKeys = (file1, file2) => {
   return _.uniq(keys);
 };
 
-export const buildDiffTree = (file1, file2) => {
+const buildDiffTree = (file1, file2) => {
   const keys = getUniqKeys(file1, file2);
   const tree = keys.map((key) => {
-    if (_.isPlainObject(file1[key] && _.isPlainObject(file2[key]))) {
-      return { key, children: getUniqKeys(file1[key], file2[key]), status: 'complex' };
+    if (_.isPlainObject(file1[key]) && _.isPlainObject(file2[key])) {
+      return { key, children: buildDiffTree(file1[key], file2[key]), status: 'complex' };
     }
     if (_.has(file1, key) && !_.has(file2, key)) {
       return { key, value: file1[key], status: 'deleted' };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import printDiffs from './flatDiff.js';
 import parseData from './parsers.js';
-import { buildDiffTree } from './flatDiff.js';
 
 const genDiff = (path1, path2) => {
   const file1 = parseData(path1);
   const file2 = parseData(path2);
   const diffs = printDiffs(file1, file2);
-  return JSON.stringify(buildDiffTree(file1, file2), null, 2);
+  return diffs;
 };
-console.log(genDiff('__fixtures__/complex1.json', '__fixtures__/complex2.json'))
+
 export default genDiff;
