cfdaefbc82bcd56ab9b9c3d6c8680a06f5cea40b
Maxim Necrasov <maddmax22@yandex.ru>
2024-03-31 21:52:59 +0300

    stylish

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,18 +1,18 @@
 import _ from 'lodash';
 
-const getSortedKeys = (file) => {
-  const keys = Object.keys(file);
+const getUniqKeys = (file1, file2) => {
+  const keys = [...Object.keys(file1), ...Object.keys(file2)];
+  return _.uniq(keys);
+};
+
+const getSortedKeys = (file1, file2) => {
+  const keys = getUniqKeys(file1, file2);
   const sorted = _.sortBy(keys);
   return sorted;
 };
 
-const getUniqKeys = (file1, file2) => {
-  const keys = [...getSortedKeys(file1), ...getSortedKeys(file2)];
-  return _.uniq(keys);
-};
-
 const buildDiffTree = (file1, file2) => {
-  const keys = getUniqKeys(file1, file2);
+  const keys = getSortedKeys(file1, file2);
   const tree = keys.map((key) => {
     if (_.isPlainObject(file1[key]) && _.isPlainObject(file2[key])) {
       return { key, children: buildDiffTree(file1[key], file2[key]), status: 'complex' };
@@ -24,7 +24,12 @@ const buildDiffTree = (file1, file2) => {
       return { key, value: file2[key], status: 'added' };
     }
     if (!_.isEqual(file1[key], file2[key])) {
-      return { key, value: file1[key], status: 'changed' };
+      return {
+        key,
+        value: file1[key],
+        value2: file2[key],
+        status: 'changed',
+      };
     }
     return { key, value: file1[key], status: 'unchanged' };
   });
--- src/stylish.js
+++ src/stylish.js
@@ -7,38 +7,43 @@ import {
 } from './utils.js';
 import buildDiffTree from './buildTree.js';
 
-const convertToString = (value) => {
+const makeSpaces = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat(depth * spacesCount - 2);
+
+const convertToString = (value, depth) => {
   if (!_.isObject(value)) {
     return `${value}`;
   }
   const entries = Object.entries(value);
-  const result = entries.map(([key, val]) => `${key}: ${convertToString(val)}`);
-  return result;
+  const result = entries.map(([key, val]) => `${makeSpaces(depth + 1)}  ${key}: ${convertToString(val, depth + 1)}`);
+  return `{\n${result.join('\n')}\n  ${makeSpaces(depth)}}`;
 };
 
 const stylish = (file1, file2) => {
   const tree = buildDiffTree(file1, file2);
-  const iter = (node) => {
+  const iter = (node, depth = 1) => {
     const lines = node.map((item) => {
       const status = getStatus(item);
+      const space = makeSpaces(depth);
       switch (status) {
         case 'complex':
-          return `${getKey(item)}: ${iter(getChildren(item))}`;
+          return `${space}  ${getKey(item)}: {\n${iter(getChildren(item), depth + 1)}\n${space}  }`;
         case 'deleted':
-          return `- ${getKey(item)}: ${convertToString(getValue(item))}`;
+          return `${space}- ${getKey(item)}: ${convertToString(getValue(item), depth)}`;
         case 'added':
-          return `+ ${getKey(item)}: ${convertToString(getValue(item))}`;
+          return `${space}+ ${getKey(item)}: ${convertToString(getValue(item), depth)}`;
         case 'changed':
-          return `- ${getKey(item)}: ${convertToString(getValue(item))} \n + ${getKey(item)}: ${convertToString(file2[getKey(item)])}`;
+          return `${space}- ${getKey(item)}: ${convertToString(getValue(item), depth)}\n${space}+ ${getKey(item)}: ${convertToString(item.value2, depth)}`;
         case 'unchanged':
-          return ` ${getKey(item)}: ${convertToString(getValue(item))}`;
+          return `${space}  ${getKey(item)}: ${convertToString(getValue(item), depth)}`;
         default:
           return 'unknown';
       }
     });
     return lines.join('\n');
   };
-  return iter(tree);
+  return `{\n${iter(tree)}\n}`;
 };
 
 export default stylish;
+
+// to do: stylish(file file) --> stylish(tree)
